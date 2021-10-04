f108dd9d01666731f18585da10b8b5c6d0b0fe18
Saif Ali Khan <saffellikhan@gmail.com>
2021-10-05 00:20:56 +0500

    Miner fixes

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,1 @@
 import { Project } from "../typescript";
-
-console.log(
-  Project.initialize({
-    name: "test",
-    description: "This is a test app.",
-    type: "Application",
-    brandName: "Epic",
-    brandCountry: "Pakistan",
-    brandAddress: "N/A",
-  })
-);
